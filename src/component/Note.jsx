f88316a760b620e9dec03aa3c7ced241ddd9adb4
Tran Tu <tranhoangtu12091994@gmail.com>
2021-05-27 10:57:22 +0700

    init

---
+++ src/component/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>This is title</h1>
+        <p>THis is conntent</p>
+    </div>
+}
+
+export default Note;
